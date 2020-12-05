5a49147751d859a75bf67ad8e30dedb68a2484f8
Aziz <aziz98366@gmail.com>
2020-12-05 14:15:42 -0800

    First Commit

---
+++ HuffmanTree.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class HuffmanTree {
+
+	public HuffmanTree(Scanner codeInput) {
+	}
+
+	public HuffmanTree(int[] count) {
+	}
+
+	public void decode(BitInputStream input, PrintStream output, int charMax) {
+	}
+
+	public void write(PrintStream output) {
+	}
+
+}
