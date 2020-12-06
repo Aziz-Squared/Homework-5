74ad593b98879f8f06d98f7ad501aaa5ecdfdf3c
Aziz <aziz98366@gmail.com>
2020-12-06 03:11:20 -0800

    Updated write, constructors and node. Unsure if it works yet, have to finish decode

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,4 +1,5 @@
 import java.io.PrintStream;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class HuffmanTree {
@@ -36,7 +37,16 @@ public class HuffmanTree {
 			this.right = n2;
 			this.frequency = n1.frequency + n2.frequency;
 		}
+		public HuffmanNode(int frequency, int ascii) {
+			this(frequency, ascii, null, null);
+		 }
 		
+		 public HuffmanNode(int frequency, int ascii, HuffmanNode left, HuffmanNode right){
+			 this.frequency = frequency;
+			 this.ascii = ascii;
+			 this.left = left;
+			 this.right = right;
+		 }
 		
 		public int compareTo(HuffmanNode h) {
 			if (this.frequency < h.frequency) {
@@ -47,10 +57,39 @@ public class HuffmanTree {
 				return 1;
 			}
 		}
+
+	}
+	
+
+	private HuffmanNode HuffmanTreeBuilder(HuffmanNode root, int letterCode, String code){
+		if (root == null) {
+		   root = new HuffmanNode(0, -1);
+		}
+		if (code.length() == 1) {
+		   if (code.charAt(0) == '0') {
+			  root.left = new HuffmanNode(0, letterCode);
+		   } else {
+			  root.right = new HuffmanNode(0, letterCode);
+		   }
+		} else {
+		   char codeValue = code.charAt(0);
+		  code = code.substring(1);
+		   if (codeValue == '0') {
+			  root.left = HuffmanTreeBuilder(root.left, letterCode, code);
+		   } else {
+			  root.right = HuffmanTreeBuilder(root.right, letterCode, code);      
+		   }
+		}
+		return root;
 	}
 
 
 	public HuffmanTree(Scanner codeInput) {
+		while (codeInput.hasNextLine()) {
+			int letterCode = Integer.parseInt(codeInput.nextLine());
+			String code = codeInput.nextLine();
+			overallRoot = HuffmanTreeBuilder(overallRoot, letterCode, code);
+		 }
 	}
 
 
@@ -58,6 +97,19 @@ public class HuffmanTree {
 	}
 
 	public void write(PrintStream output) {
+		if(overallRoot != null){
+			write(output, overallRoot, "");
+		}
 	}
+
+	private void write(PrintStream output, HuffmanNode root, String code) {
+		if (root.left == null && root.right == null) {
+		   output.println(root.ascii);
+		   output.println(code);
+		} else {
+		   write(output, root.left, code + "0");
+		   write(output, root.right, code + "1"); 
+		}
+	 }
 
 }
