f8d62004b2f0ea98e4854d9c927f2818f2a62ce7
Aziz <aziz98366@gmail.com>
2020-12-09 01:19:40 -0800

    I think it's finished?

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -2,101 +2,119 @@ import java.io.PrintStream;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+import jdk.jfr.Frequency;
+
 public class HuffmanTree {
-	
+
 	private HuffmanNode overallRoot;
-	
-	
+
 	public HuffmanTree(int[] count) {
-		PriorityQueue <HuffmanNode> pQ = new PriorityQueue <HuffmanNode> ();
-		
-		for (int i = 1; i < count.length; i++) {
+		PriorityQueue<HuffmanNode> frequencyCounter = new PriorityQueue<HuffmanNode>();
+
+		for (int i = 0; i < count.length; i++) {
 			if (count[i] > 0) {
-				pQ.offer(new HuffmanNode(count[i], i);
+				HuffmanNode freqRate = new HuffmanNode(count[i], i);
+				frequencyCounter.add(freqRate);
 			}
 		}
-		
-		while (pQ.size() >= 2) {
-			HuffmanNode node1 = pQ.poll();
-			HuffmanNode node2 = pQ.poll();
-			pQ.offer(new HuffmanNode(node1.frequency + node2.frequency, -1, node1, node2));
+		HuffmanNode eof = new HuffmanNode(1, count.length);
+		frequencyCounter.add(eof);
+		while (frequencyCounter.size() != 1) {
+			HuffmanNode current = frequencyCounter.remove();
+			HuffmanNode next = frequencyCounter.remove();
+			// nodes representing a sum of counts with a character number of -1
+			HuffmanNode sum = new HuffmanNode(current.frequency + next.frequency, -1, current, next);
+			frequencyCounter.add(sum);
 		}
+		overallRoot = frequencyCounter.remove();
 	}
-	
-	
-	private class HuffmanNode implements Comparable <HuffmanNode> {
+
+	private class HuffmanNode implements Comparable<HuffmanNode> {
 		public int frequency;
 		public int ascii;
 		public HuffmanNode left, right;
-		
-		
+
 		public HuffmanNode(int frequency, int ascii) {
 			this(frequency, ascii, null, null);
-		 }
-		
-		 public HuffmanNode(int frequency, int ascii, HuffmanNode left, HuffmanNode right){
-			 this.frequency = frequency;
-			 this.ascii = ascii;
-			 this.left = left;
-			 this.right = right;
-		 }
-		
+		}
+
+		public HuffmanNode(int frequency, int ascii, HuffmanNode left, HuffmanNode right) {
+			this.frequency = frequency;
+			this.ascii = ascii;
+			this.left = left;
+			this.right = right;
+		}
+
 		public int compareTo(HuffmanNode h) {
 			return this.frequency - h.frequency;
 		}
 
 	}
-	
 
-	private HuffmanNode HuffmanTreeBuilder(HuffmanNode root, int letterCode, String code){
+	private HuffmanNode HuffmanTreeBuilder(HuffmanNode root, int letterCode, String code) {
 		if (root == null) {
-		   root = new HuffmanNode(0, -1);
+			root = new HuffmanNode(0, -1);
 		}
 		if (code.length() == 1) {
-		   if (code.charAt(0) == '0') {
-			  root.left = new HuffmanNode(0, letterCode);
-		   } else {
-			  root.right = new HuffmanNode(0, letterCode);
-		   }
+			if (code.charAt(0) == '0') {
+				root.left = new HuffmanNode(0, letterCode);
+			} else {
+				root.right = new HuffmanNode(0, letterCode);
+			}
 		} else {
-		   char codeValue = code.charAt(0);
-		  code = code.substring(1);
-		   if (codeValue == '0') {
-			  root.left = HuffmanTreeBuilder(root.left, letterCode, code);
-		   } else {
-			  root.right = HuffmanTreeBuilder(root.right, letterCode, code);      
-		   }
+			char codeValue = code.charAt(0);
+			code = code.substring(1);
+			if (codeValue == '0') {
+				root.left = HuffmanTreeBuilder(root.left, letterCode, code);
+			} else {
+				root.right = HuffmanTreeBuilder(root.right, letterCode, code);
+			}
 		}
 		return root;
 	}
-
 
 	public HuffmanTree(Scanner codeInput) {
 		while (codeInput.hasNextLine()) {
 			int letterCode = Integer.parseInt(codeInput.nextLine());
 			String code = codeInput.nextLine();
 			overallRoot = HuffmanTreeBuilder(overallRoot, letterCode, code);
-		 }
+		}
 	}
 
+	public void decode(BitInputStream input, PrintStream output, int eofValue) {
+		int charCode = input.readBit();
+		while (charCode != -1) {
+			charCode = HuffmanTreeTraverse(input, charCode, overallRoot, output, eofValue);
+		}
+	}
 
-	public void decode(BitInputStream input, PrintStream output, int charMax) {
+	private int HuffmanTreeTraverse(BitInputStream input, int code, HuffmanNode root, PrintStream output,
+			int eofValue) {
+		if (root.left == null && root.right == null && root.ascii != eofValue) {
+			output.write(root.ascii);
+			return code;
+		} else if (code == 0 && root.ascii != eofValue) {
+			return HuffmanTreeTraverse(input, input.readBit(), root.left, output, eofValue);
+		} else if (root.ascii != eofValue) {
+			return HuffmanTreeTraverse(input, input.readBit(), root.right, output, eofValue);
+		}
+		return -1;
 	}
 
 	public void write(PrintStream output) {
-		if(overallRoot != null){
+		if (overallRoot != null) {
 			write(output, overallRoot, "");
 		}
 	}
 
 	private void write(PrintStream output, HuffmanNode root, String code) {
 		if (root.left == null && root.right == null) {
-		   output.println(root.ascii);
-		   output.println(code);
+			output.println(root.ascii);
+			output.println(code);
 		} else {
-		   write(output, root.left, code + "0");
-		   write(output, root.right, code + "1"); 
+			write(output, root.left, code + "0");
+			write(output, root.right, code + "1");
 		}
-	 }
+	}
 
 }
